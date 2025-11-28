784facfe375724043f224c0090be7f3834229c84
radhasaik2 <radhasaik.2@gmail.com>
2025-11-28 17:41:02 +0530

    RestApi Jpa React-SpringBoot Project MovietktApplication.java

---
+++ RestApi Jpa React-SpringBoot Project/Back-End/src/main/java/com.sat.tmf.movietkt/MovietktApplication.java
@@ -0,0 +1,11 @@
+package com.sat.tmf.movietkt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovietktApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MovietktApplication.class, args);
+    }
+}
